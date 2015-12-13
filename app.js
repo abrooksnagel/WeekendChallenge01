01d7d4f1cd3850ce93d2b4a868c73e7a9880f234
abrooksnagel <abrooksnagel@Aarons-MacBook-Air.local>
2015-12-13 11:31:09 -0600

    Make the button actually work to delete name

--- app.js
+++ app.js
@@ -6,25 +6,23 @@ $(document).ready(function () {
 	$('#employeeInfo').on('submit', function(event) {
 
 //this removes the previous entry from the DOM for ease of viewing
-		$("p").remove();
-		$("h4").remove();
-
-//this sometimes removes employees on button click
-		$("h3").on('click', function () {
-			$(this).remove();
-		});
+		//$("p").remove();
+		$("h4").remove();		
 
 
 //this prevents the information from disappearing on submit
 		event.preventDefault();
 
+
 //this creates an empty object
 		var values = {};
+
 
 //this fills the object with input values?
  		$.each($("#employeeInfo").serializeArray(), function(i, field){
 			values[field.name] = field.value;
 		})
+
 
 //this takes the value: employeeSalary, adds it to totalSalary and breaks down into monthly cost
 		var salary = parseInt(values.employeeSalary);
@@ -33,18 +31,21 @@ $(document).ready(function () {
 		console.log(totalSalary);
 		console.log(monthlyCost);
 
+
 //this removes the input values once we put them into the object
  		$('#employeeInfo').find('input[type=number]').val('');
 		$('#employeeInfo').find('input[type=text]').val('');
 
+
 //this calls the function appendDOM using values as the argument
-		//console.log(values);
+		console.log(values);
 		appendDOM(values);
 
 
 //this pushes the object values to the array
 		array.push(values);
-		//console.log(array);
+		console.log(array);
+
 
 //this is a previous attempt that may have worked with some tweaking that I want to keep for future experimentation
 		//for (var i = 0; i < array.length; i++) {
@@ -52,7 +53,6 @@ $(document).ready(function () {
 		//	totalSalary += currentSalary;
 		//	console.log(totalSalary);
 	    //}
-
 
 
 //I moved this into the ready function to help with the delete button
@@ -66,6 +66,17 @@ function appendDOM(object) {
 	$el.append("<p>Employee Salary (Yearly): $" + object.employeeSalary + "</p>");
 	$el.append("<h4>Total Monthly Payroll Expenditures: $" + monthlyCost + "</h4>");
 };
+
+
+//this sometimes removes employees' names <h3> on button click 
+//but not <p> or <h4> but I don't know why
+
+		$("button").on('click', function () {			
+			$(this).closest("h3").addClass("clicked");
+			$(this).closest("p").addClass("clicked");
+			$(this).closest("h4").addClass("clicked");
+		});
+
 
 	});	
 });
